9cf93b49a3410235b42cdb7bdbf6bd470fd5a19c
joaopedrolour3nc <joaopedro08moura@gmail.com>
2024-05-16 19:49:16 -0300

    Aulas 8 e 9

--- aula7.py
+++ aula7.py
@@ -1,7 +1,4 @@
-nome_completo = 'João Pedro Moura'
-soma_dois_mais_dois = 2 + 2
-int_um = bool('1')
-
-print(int_um, type(int_um))     
-print(nome_completo, soma_dois_mais_dois)
-
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+print('nome:', nome,"maior de idade, pois ele tem", idade)
---
+++ aula9.py
@@ -0,0 +1,11 @@
+adicao = 10 + 10
+print('Adição: ', adicao)   
+
+subtracao = 10 -5
+print("Subtração: ", subtracao)
+
+multiplicacao = 10 * 6
+print("Multiplicação: ", multiplicacao)
+
+divisao = multiplicacao / 2
+print('Divisão: ' ,divisao)
