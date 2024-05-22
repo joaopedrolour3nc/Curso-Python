8a92bfaf9d9a26760ba5fa69971d80341e5733ff
joaopedrolour3nc <joaopedro08moura@gmail.com>
2024-05-21 23:54:10 -0300

    Aulas até 14

---
+++ aula13.py
@@ -0,0 +1,13 @@
+nome = 'João'
+peso = 55
+altura = 1.83
+
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura:.2f} de altura' 
+linha_2 = f'pesa {peso} Kilos e o seu IMC é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3 )
---
+++ aula14.py
@@ -0,0 +1,8 @@
+a = 'A'
+b = 'B'
+c = 1.144884
+
+string ='a={1} b={2} c={2:.2f}'
+formato = string.format(a, b, c)
+
+print(formato)
