9055b4cab21bbba94197bd7bc6a3c60a5cb4d757
joaopedrolour3nc <joaopedro08moura@gmail.com>
2024-06-05 19:55:37 -0300

    mais aulas 15 e 16

---
+++ aula16.py
@@ -0,0 +1,11 @@
+#If / elif   / else
+#se/ se não se/ se não
+
+entrada = input('Você quer entrar ou sair?: ')
+
+if(entrada == "entrar"):
+    print("Você entrou")
+elif(entrada == "sair"):
+    print("Você saiu")
+else:
+    print("Você não sabe fazer nada")
