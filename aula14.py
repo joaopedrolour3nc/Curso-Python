e4e763775e08d438e99a84ff0f744fa2d78041a4
joaopedrolour3nc <joaopedro08moura@gmail.com>
2024-07-25 15:16:24 -0300

    Aula sobre a função Formart e Inicio de Indice {}

--- aula14.py
+++ aula14.py
@@ -1,8 +1,13 @@
-a = 'A'
+# Out of Range é que eu to buscando a mais do que eu tenho
+# a={Aqui é o Indice que vou querer mostrar, mas preciso lembrar que começa no 0}
+# nome3=c é um parâmetro nomeado
+
+
+a = 'AAAAAAA'
 b = 'B'
 c = 1.144884
 
-string ='a={1} b={2} c={2:.2f}'
+string ='a={0} a={0} a={0} a={0} b={1} c={2:.2f}'
 formato = string.format(a, b, c)
 
 print(formato)
