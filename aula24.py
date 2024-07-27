4bee6eb9edb07a7ec78c3be41afea7e7c834bdba
joaopedrolour3nc <joaopedro08moura@gmail.com>
2024-07-27 16:25:38 -0300

    Aula sobre Operadores In e Not in

--- aula24.py
+++ aula24.py
@@ -3,9 +3,15 @@ Operadores in e not in
 Strings são iteraveis 
 
 """
+"""
+Iteravel é um  negócio que você pode navegar item por item
 
+Começa do zero
+ 0  1  2  3  4  5
+ o  t  a  v  i  o
+-6 -5 -4 -3 -2 -1
+"""
 # nome = 'otavio'
-
 # print(nome[2])
 
 # print('a' in nome)
