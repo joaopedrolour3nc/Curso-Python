4feb759724ff434ec19c0cdfa2fc0fb936617f1c
joaopedrolour3nc <joaopedro08moura@gmail.com>
2024-07-28 00:06:37 -0300

    Aula sobre Fatiamento de String com a função Len

--- aula27.py
+++ aula27.py
@@ -1,4 +1,11 @@
-# A letra control e ´ Duplica a linha 
+# Ctrl + 'duplica a Linha
+# Fatiamento de String
+# 012345678
+# Olá Mundo 
+#-987654321
+# print(varave[Inicio:Final:passo])  
+# Função Len retorna a qtd de caracter que tem
+
 
 varave = 'Olá Mundo'
-print(varave[-8:-3])
+print(len(varave))
