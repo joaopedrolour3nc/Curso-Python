431f16cbc1fb2d91ac3ebe1076bbbff9d4dad699
joaopedrolour3nc <joaopedro08moura@gmail.com>
2024-11-13 23:43:42 -0300

    Aula sobre Fatiamento e Len

--- aula27.py
+++ aula27.py
@@ -3,9 +3,12 @@
 # 012345678
 # Olá Mundo 
 #-987654321
-# print(varave[Inicio:Final:passo])  
+# print(varave[Inicio:Final:passo]) [::]  
 # Função Len retorna a qtd de caracter que tem
+# 0:9:-1 o negativo faz eu exibir ao contrario
 
 
-varave = 'Olá Mundo'
-print(len(varave))
+
+variavel = 'Olá Mundo'
+print(variavel[:])
+print(variavel[::-1])
