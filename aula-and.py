665d1e49a966037247097a81baa1d5915e7b8344
João Pedro <joaopedro08moura@gmail.com>
2025-06-17 18:47:18 -0300

    Removendo arquivos da branch errada

---
+++ aula-and.py
@@ -0,0 +1,19 @@
+"""
+And todas as condições precisam ser verdadeiras
+"""
+
+# entrada = input("[E]ntrar [S]air: ")
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print("Entrar")
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+print(True and False and True)
+
+
+# Atalho para comentário Ctrl + K, Ctrl + C
+
